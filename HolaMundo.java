f3f324fc51f75cb9af636f7522bcc4f5a6157ca4
Joseph Torres <j.torrescerbellon@gmail.com>
2023-04-24 17:27:21 -0400

    First commit

---
+++ HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Mi Nombre es Coding Dojo");
+        System.out.println("Tengo 10 años de edad.");
+        System.out.println("Mi ciudad es Burbank, CA");
+    }
+}
